1d9389efdecd323ea4f4619b5d49389dad2c8577
ZongboXu <zongboxu@hotmail.com>
2017-10-31 19:44:52 -0600

    Add files via upload

--- lovefv2.m
+++ lovefv2.m
@@ -6,39 +6,43 @@ mu1 = 8E7;
 mu2 = 1.28E9;
 beta1 = 200; beta2 = 800; H = 40;
 
-dc = 0.9;
-middv = 0.1;
-fres = 1:1:100;
-fv = zeros(length(fres),2);
+dc = 0.00001;
+middv = dc/10;
+fres = 4;
+fv = zeros(length(fres),3);
 fv(:,1) = fres';
 
 fun = @(vel,omega) tan(omega*H*sqrt(1/(beta1^2)-1/(vel^2)))-mu2/mu1*sqrt(1/(vel^2)-1/(beta2^2))/sqrt(1/(beta1^2)-1/(vel^2));
+tanomega = @(vel,omega) tan(omega*H*sqrt(1/(beta1^2)-1/(vel^2)));
 
 for ifre = 1 : length(fres) 
-    fre = fres(ifre);
-    omega = 2 * pi *fre;
+    omega = 2 * pi *fres(ifre);
+    imode = 0;
     for c = 1+beta1 : dc : beta2 - dc
         a = c; b = c + dc;
         funa = fun(a,omega);
         funb = fun(b,omega);
-        if funa*funb > 0
+        if funa*funb > 0 
             continue;
-        else
+        elseif tanomega(b,omega) - tanomega(a,omega)>0
             while abs(a-b) > middv
               midab = (a+b)/2;
               funmid = fun(midab,omega);
-              if funa * funmid > 0
+              if funa * funmid > 0 
                   a = midab;
               else
                   b = midab;
               end
             end
-            fv(ifre,2) = (a+b)/2;
-            break;
+            fv(ifre,imode+2) = (a+b)/2;
+            imode = imode + 1;
+            if imode > 2 
+                break
+            end
+            continue;
         end
     end
             
 end
 
 end
-
